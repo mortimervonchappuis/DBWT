36a09bc90df0bbb0d79b5eecc5e9abcbfeaa5a36
mortimer <mortimervonchappuis@protonmail.com>
2020-11-19 01:13:52 +0100

    Aufgaben 1, 2, 3, 4

---
+++ meilenstein_3/emensawerbeseite_DDL.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS emensawerbeseite;
+CREATE DATABASE IF NOT EXISTS emensawerbeseite;
+USE emensawerbeseite;
+DROP TABLE IF EXISTS gericht;
+DROP TABLE IF EXISTS allergen;
+DROP TABLE IF EXISTS kategorie;
+DROP TABLE IF EXISTS gericht_hat_allergen;
+DROP TABLE IF EXISTS gericht_hat_kategorie;
+SET NAMES utf8mb4;
+
+CREATE TABLE gericht(
+  id INT(8),
+  name VARCHAR(80) NOT NULL,
+  beschreibung VARCHAR(80) NOT NULL,
+  erfasst_am DATE NOT NULL,
+  vegetarisch BOOLEAN NOT NULL,
+  vegan BOOLEAN NOT NULL,
+  preis_intern DOUBLE NOT NULL CHECK(preis_intern <= preis_extern),
+  preis_extern DOUBLE NOT NULL CHECK(preis_intern <= preis_extern),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE allergen(
+  code CHAR(4),
+  name VARCHAR(300) NOT NULL,
+  typ VARCHAR(20) NOT NULL,
+  PRIMARY KEY (code)
+);
+
+CREATE TABLE kategorie(
+  id INT(8) NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  eltern_id INT(8),
+  bildname VARCHAR(200),
+  FOREIGN KEY (eltern_id) REFERENCES kategorie(id),
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE gericht_hat_allergen(
+  code CHAR(4),
+  gericht_id INT(8) NOT NULL,
+  FOREIGN KEY (code) REFERENCES allergen(code),
+  FOREIGN KEY (gericht_id) REFERENCES gericht(id),
+  PRIMARY KEY (code, gericht_id)
+);
+
+CREATE TABLE gericht_hat_kategorie(
+  gericht_id INT(8),
+  kategorie_id INT(8),
+  FOREIGN KEY (gericht_id) REFERENCES gericht(id),
+  FOREIGN KEY (kategorie_id) REFERENCES kategorie(id),
+  PRIMARY KEY (gericht_id, kategorie_id)
+);
---
+++ meilenstein_3/emensawerbeseite_DQL.sql
@@ -0,0 +1,7 @@
+USE emensawerbeseite;
+
+SELECT COUNT(*) FROM gericht;
+SELECT COUNT(*) FROM allergen;
+SELECT COUNT(*) FROM kategorie;
+SELECT COUNT(*) FROM gericht_hat_allergen;
+SELECT COUNT(*) FROM gericht_hat_kategorie;
