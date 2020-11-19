d16f3fae82e3cacd62d29fbfa431a9e7287da5fc
mortimer <mortimervonchappuis@protonmail.com>
2020-11-19 18:17:00 +0100

    Aufgabe 6 fertig

--- meilenstein_3/beispiele/m3_8a_testdatenbank.php
+++ meilenstein_3/beispiele/m3_8a_testdatenbank.php
@@ -1,13 +1,54 @@
-<?php
-/**
- * Praktikum DBWT. Autoren:
- * Dominik, Bien, 3149135
- * Botho Karl Mortimer, von Chappuis, 3146023
- * Date: 11/19/20
- * Time: 5:11 PM
- */
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta CHARSET="UTF-8">
+            <title>Aufgabe 6</title>
+        </head>
+        <body>
+            <main>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Gericht</td>
+                            <td>Zutaten</td>
+                            <td>Datum</td>
+                            <td>Vegetarisch</td>
+                            <td>Vegan</td>
+                            <td>Preis intern</td>
+                            <td>Preis extern</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    /**
+                     * Praktikum DBWT. Autoren:
+                     * Dominik, Bien, 3149135
+                     * Botho Karl Mortimer, von Chappuis, 3146023
+                     * Date: 11/19/20
+                     * Time: 5:11 PM
+                     */
 
-$port = 3306;
-$link = mysqli_connect("127.0.0.1", "root", "root","emensawerbeseite", $port);
-$query = "";
-$result = mysqli_query($link, $query);
+                    $port = 3306;
+                    $link = mysqli_connect("127.0.0.1", "root", "root","emensawerbeseite", $port);
+                    $query = "SELECT * FROM gericht;";
+                    $result = mysqli_query($link, $query);
+                    while ($row = mysqli_fetch_row($result)){
+                        echo "<tr>";
+                        echo "<td>".$row[0]."</td>\n";
+                        echo "<td>".$row[1]."</td>\n";
+                        echo "<td>".$row[2]."</td>\n";
+                        echo "<td>".$row[3]."</td>\n";
+                        echo "<td>".($row[4] ? '&#x2611;' : '&#x2612;')."</td>\n";
+                        echo "<td>".($row[5] ? '&#x2611;' : '&#x2612;')."</td>\n";
+                        echo "<td>".number_format($row[6],2)."€</td>\n";
+                        echo "<td>".number_format($row[7],2)."€</td>\n";
+                        echo "</tr>\n";
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </main>
+        </body>
+    </html>
+</DOCTYPE>
