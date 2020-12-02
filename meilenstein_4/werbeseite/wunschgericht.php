1f5ec1f31d8a833109f812ae755ae6749388e49c
mortimer <mortimervonchappuis@protonmail.com>
2020-12-03 00:17:17 +0100

    Aufgabe 1

--- meilenstein_4/werbeseite/wunschgericht.php
+++ meilenstein_4/werbeseite/wunschgericht.php
@@ -7,8 +7,24 @@
  * Time: 9:10 PM
  */
 include("db_stuff.php");
-$query = "SELECT * FROM wunschgericht;";
-$result = mysqli_query($link, $query);
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $verfasser = trim($_POST['verfasser']);
+    if ($verfasser == ''){
+        $verfasser = 'anonym';
+    }
+    $email = $_POST['e-mail'];
+    $name = $_POST['name'];
+    $beschreibung = $_POST['beschreibung'];
+
+
+    $query = "INSERT INTO wunschgericht(Name, Beschreibung, Erstelldatum) VALUES ('".$name."', '".$beschreibung."', NOW());";
+    $result = mysqli_query($link, $query);
+    $query = "INSERT INTO erstellerin(Name, E_Mail) SELECT * FROM (SELECT '".$verfasser."', '".$email."') AS tmp 
+    WHERE NOT EXISTS (SELECT Name FROM erstellerin WHERE E_Mail = '".$email."') LIMIT 1;";
+    $result = mysqli_query($link, $query);
+    $query = "INSERT INTO wunschgericht_hat_erstellerin(erstellerin, wunschgericht_id) SELECT '".$email."', id FROM wunschgericht WHERE Name = '".$name."';";
+    $result = mysqli_query($link, $query);
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -26,101 +42,17 @@ $result = mysqli_query($link, $query);
         <div class="picture-placeholder">
             <img class=banner src="banner.png" alt='nö'>
         </div>
-        <div id="neuigkeiten">
-            <h2 class="headline-bold">Bald gibt es Essen auch online ;)</h2>
-            <p id="fliesstext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-        </div>
-        <!--Preis tabelle-->
-        <div class="preis" id="speisen">
-            <h2 class="headline-bold">Köstlichkeiten, die Sie erwarten!</h2>
-
-            <?php
-            include($gerichte_file);
-            ?>
-
-        </div>
-
-        <div class="counter" id="counter">
-            <h2 class="headline-bold">E-Mensa in Zahlen</h2>
-            <ul class="counter-grid">
-                <li class="counter-var">
-                    <?php include "visit_count.php" ?>
-                </li>
-                <li class="counter-name">
-                    Besuche
-                </li>
-                <li class="counter-var">
-                    <?= number_of_newsletter_subscribers() ?>
-                </li>
-                <li class="counter-name">
-                    Anmelungen zum Newsletter
-                </li>
-                <li class="counter-var">
-                    <?= number_of_meals() ?>
-                </li>
-                <li class="counter-name">
-                    Speisen
-                </li>
-            </ul>
-        </div>
-
-        <div class="newsletter" id="newsletter">
-            <h2 class="headline-bold">Interesse geweckt? Wir informieren Sie!</h2>
-            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-                <div class=row>
-                    <div class=col>
-                        <p class="newsletter-vorname">
-                            <label for="name">Ihr Name:</label><br>
-                            <input type="text"  id="name" name="name" placeholder="Name" value="<?= $name ?>">
-                            <span class="error"> <?= $name_error?></span>
-                        </p>
-                    </div>
-                    <div class=col>
-                        <p class="newsletter-email">
-                            <label for="email">Ihre E-Mail:</label><br>
-                            <input type="text" id="email" name="email" placeholder="E-Mail" value="<?= $email ?>">
-                            <span class="error"> <?= $email_error?></span>
-                        </p>
-                    </div>
-                    <div class=col>
-                        <p class="newsletter-language">
-                            <label for="language">Newsletter bitte in:</label><br>
-                            <select id="language" name="language">
-                                <option value="de">Deutsch</option>
-                                <option value="en">Englisch</option>
-                            </select>
-                        </p>
-                    </div>
-                </div>
-                <div class=row>
-                    <div class=col>
-                        <p class="datenschutz">
-                            <input type="checkbox" id="datenschutz" name="datenschutz">
-                            <label for="datenschutz">Den Datenschutzbestimmungen stimme ich zu</label>
-                        </p>
-                        <span class="error"> <?= $datenschutz_error?></span>
-                    </div>
-                    <div class=col>
-                        <p class="submit">
-                            <input type="submit" value="Zum Newsletter anmelden">
-                        </p>
-                    </div>
-                </div>
-            </form>
-        </div>
-
-        <div class="fakenews" id="info">
-            <h2 class="headline-bold">Das ist uns wichtig!!!</h2>
-            <ul >
-                <li>Beste frische saisonale Zutaten</li>
-                <li>Ausgewogene abwechslungsreiche Gerichte</li>
-                <li>Sauberkeit</li>
-            </ul>
-        </div>
-
-        <h2 class="last-headline-bold">Wir freuen uns auf Ihren Besuch!</h2>
+        <form action="wunschgericht.php" method="POST">
+            <label for="verfasser">Verfasser</label><br>
+            <input name="verfasser"><br>
+            <label for="e-mail">E-Mail</label><br>
+            <input required name="e-mail" type="email"><br>
+            <label for="name">Name des Gerichts</label><br>
+            <input required name="name"><br>
+            <label for="beschreibung">Beschreibung</label><br>
+            <textarea name="beschreibung"></textarea><br>
+            <button type="submit">Wunsch abschicken</button>
+        </form>
     </div>
 </main>
 <?php include("parts/footer.html"); ?>
