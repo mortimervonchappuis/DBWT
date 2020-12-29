42389fc094066328d8f290585f7027b3cfd3cad9
mortimer <mortimervonchappuis@protonmail.com>
2020-12-29 04:45:16 +0100

    typos fixed

--- meilenstein_4/emensa/app/Http/Controllers/EmensaController.php
+++ meilenstein_4/emensa/app/Http/Controllers/EmensaController.php
@@ -9,7 +9,7 @@ class EmensaController extends Controller
 
     public function indexGerichte()
     {
-        $mealList = DB::select("SELECT g.name, g.preis_intern, g.preis_extern, GROUP_CONCAT(gha.code) as gha_code FROM gericht as g JOIN gericht_hat_allergen AS gha ON gha.gericht_id = g.id GROUP BY g.id ;");
+        $mealList = DB::raw("SELECT g.name, g.preis_intern, g.preis_extern, GROUP_CONCAT(gha.code) as gha_code FROM gericht as g JOIN gericht_hat_allergen AS gha ON gha.gericht_id = g.id GROUP BY g.id ;");
 
         return view('Homepage/meals',['mealList'=>$mealList]);
     }
